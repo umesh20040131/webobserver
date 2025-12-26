6ac8cc101720ee72081c7682f5282204db03bfc0
umesh20040131 <uchaursiya6@gmail.com>
2025-12-26 13:18:54 +0530

    first

--- app/configs/schema.ts
+++ app/configs/schema.ts
@@ -1,7 +1,17 @@
+import { boolean } from "drizzle-orm/pg-core";
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const websitesTable = pgTable("websites", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  websiteId: varchar({ length: 255 }).notNull().unique(),
+  domain: varchar({ length: 255 }).notNull().unique(),
+  timezone: varchar({ length: 255 }).notNull(),
+  enablelocalhostTracling: boolean().default(false),
+  userEmail:varchar ({length:255}).notNull(),
 });
